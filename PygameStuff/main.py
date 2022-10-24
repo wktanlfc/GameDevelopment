cbb052dff5f1831ee74e376af84f4b832fcc4725
kiang <weekiangtan2015@gmail.com>
2022-10-24 21:24:33 +0800

    Main Driver

--- PygameStuff/main.py
+++ PygameStuff/main.py
@@ -5,30 +5,27 @@ import time
 from SpriteSheet import SpriteSheet
 from Protagonist import Protagonist
 from pytmx import load_pygame
-# from store_sprite_instances import store_sprite_instances
+from Camera import CameraGroup
 
-WIDTH = 1280
-HEIGHT = 1000 #720
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+WIDTH = 1400
+HEIGHT = 1000
+WIN = pygame.display.set_mode((WIDTH, HEIGHT)) # Window Size
 pygame.display.set_caption("Valley Map")
-
+scroll = [0 , 0]
 
 #Load Images
 main_map = pygame.image.load(
     "/Users/kiang/PycharmProjects/pythonProject/PygameStuff/levels/TiledMapScenario1.png").convert()  # surface
-# sample = pygame.transform.scale(sample, (WIDTH, HEIGHT))
 
 # Load sprites
-all_sprites = pygame.sprite.Group()
+MyCamera = CameraGroup(main_map) # sprite.Group subclass + surface_param for main map.
 player = Protagonist()
-all_sprites.add(player)
+MyCamera.add(player)
+
 
 def redraw():
-    WIN.fill((255, 255, 255))
-    WIN.blit(main_map, (-1200 - player.rect.x, -1200 - player.rect.y)) #you want your mainmap (3200x3200) to be located x-axis from the left -500 -750 -1350 , -1750
-    all_sprites.draw(WIN)
-    # WIN.blit(player.image, player.rect)
-    all_sprites.update()  # Update Protagonist sprite position
+    MyCamera.update()
+    MyCamera.custom_draw(player)
     pygame.display.update()  # Update screen position
 
 def main():
@@ -36,8 +33,12 @@ def main():
     FPS = 60
     clock = pygame.time.Clock()
     while run:
+        # print(type(player))
+        # print('Display screen size is :' + str(MyCamera.display_surface))
+        # print('Protagonist is at' + str(player.rect)) # Adding it here lets you see the coordinates of where your character is.
+        # print(MyCamera.main_map_rect.center)
         clock.tick(FPS)  # Gets you the time in milliseconds since pygame.init() was called
-        print(player.rect) #Adding it here lets you see the coordinates of where your character is.
+
         # Check events, can look up events in the pygame website
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -58,7 +59,6 @@ def main():
                     player.facing_left, player.facing_up, player.facing_down, player.facing_right = False, False, True, False
                 if event.key == pygame.K_SPACE:
                     player.move_left, player.move_up, player.move_down , player.move_right, player.attacking = False, False, False, False, True
-                
                 if event.type == pygame.KEYUP:
                     if event.key == pygame.K_a:
                         player.move_left , player.facing_left = False, True
@@ -70,7 +70,7 @@ def main():
                         player.move_down , player.facing_down = False, True
                     if event.key == pygame.K_SPACE:
                         player.attacking = False
-                    
+            
             redraw()
-
+            
 main()
